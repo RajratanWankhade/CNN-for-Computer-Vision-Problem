f804134f6bb508558d0a1c3438f6efd11b68ba94
Rajratan Wankhade <rajratan.wankhade.dcu@gmail.com>
2024-07-14 12:00:44 +0100

    Data Ingestion

--- template.py
+++ template.py
@@ -1,9 +1,10 @@
-import os
-from pathlib import Path
-import logging 
-
 #logging string: Use logging string instead of print statement. 
 #format is basically timestamp and the message it has,
+import os
+from pathlib import Path
+import logging
+
+#logging string
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name = 'cnnClassifier'
@@ -14,26 +15,28 @@ list_of_files = [
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/config/__init__.py",
-    f"src/{project_name}/config/configuration/__init__.py",
+    f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/constants__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html",
-    "test.py"
+    "templates/index.html"
+
 
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-    if filedir != "":
+
+    if filedir !="":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory; {filedir} for the file: {filename}")
 
@@ -42,6 +45,6 @@ for filepath in list_of_files:
             pass
             logging.info(f"Creating empty file: {filepath}")
 
-    else:
-        logging.info(f"{filename} is already exists")             
 
+    else:
+        logging.info(f"{filename} is already exists")
